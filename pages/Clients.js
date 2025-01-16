ec62a5325777c74280b4075d8424c6295f8770e2
Dorsa Momeni <2668095m@student.gla.ac.uk>
2025-01-16 14:24:21 +0000

    coach: client page frontend completed

--- pages/Clients.js
+++ pages/Clients.js
@@ -1,25 +1,9 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import ClientsTabs from "./ClientsTabs";
 
 const Clients = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Clients</Text>
-    </View>
-  );
+  return <ClientsTabs />;
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#fff",
-  },
-  title: {
-    fontSize: 32,
-    fontWeight: "bold",
-  },
-});
 
 export default Clients;
---
+++ pages/ClientsTabs.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import ClientsList from "./ClientsList";
+import ClientsStats from "./ClientsStats";
+import ClientsSettings from "./ClientsSettings";
+import { TouchableOpacity, StyleSheet } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+const Tab = createBottomTabNavigator();
+
+const CustomTabBarButton = ({ children, onPress }) => (
+  <TouchableOpacity style={styles.tabButton} onPress={onPress}>
+    {children}
+  </TouchableOpacity>
+);
+
+const ClientsTabs = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: { display: "flex" },
+        tabBarLabelStyle: { display: "none" },
+      }}
+    >
+      <Tab.Screen
+        name="Clients List"
+        component={ClientsList}
+        options={{
+          tabBarButton: (props) => (
+            <CustomTabBarButton {...props}>
+              <Icon name="users" size={24} color="#000" />
+            </CustomTabBarButton>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Stats"
+        component={ClientsStats}
+        options={{
+          tabBarButton: (props) => (
+            <CustomTabBarButton {...props}>
+              <Icon name="bar-chart" size={24} color="#000" />
+            </CustomTabBarButton>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={ClientsSettings}
+        options={{
+          tabBarButton: (props) => (
+            <CustomTabBarButton {...props}>
+              <Icon name="cog" size={24} color="#000" />
+            </CustomTabBarButton>
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+const styles = StyleSheet.create({
+  tabButton: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    marginHorizontal: 5,
+  },
+});
+
+export default ClientsTabs;
